c3959db508f35f469b0da0719ea7e818b3b97d3a
Kundan <codecracker454>
2024-04-20 15:29:08 +0530

    frontend app done

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { NotificationProvider } from "web3uikit";
+import { MoralisProvider } from "react-moralis";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { useRouter } from "next/router";
+
+//Initializar Apollo
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import "../styles/globals.css";
+import MainLayout from "../layout/mainLayout";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://api.studio.thegraph.com/query/45112/chainlink-hackathon-notional/version/latest",
+});
+
+const MyApp = ({ Component, pageProps, session }) => {
+  const router = useRouter();
+
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <ApolloProvider client={client}>
+        <MainLayout>
+          <NotificationProvider>
+            <Component {...pageProps} />
+          </NotificationProvider>
+        </MainLayout>
+      </ApolloProvider>
+    </MoralisProvider>
+  );
+};
+
+export default MyApp;
